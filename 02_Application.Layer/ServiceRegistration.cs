641a6d816690e878b16612e0d12e2c480ffc1e01
ahmetmanco <114330172+ahmetmanco@users.noreply.github.com>
2025-05-25 21:45:37 +0300

    update-2

--- 02_Application.Layer/ServiceRegistration.cs
+++ 02_Application.Layer/ServiceRegistration.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using MediatR;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 namespace _02_Application.Layer
 {
     public static class ServiceRegistration
     {
-        public static void AddApplicationServices(this IServiceCollection serviceCollection)
+        public static void AddApplicationServices(this Microsoft.Extensions.DependencyInjection.IServiceCollection serviceCollection)
         {
             serviceCollection.AddMediatR(typeof(ServiceRegistration));
         }
